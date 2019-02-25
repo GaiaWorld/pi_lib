f11e3ec474acc353acb62a373f20b77358abc7b6
vmos <vmos@fairblock.io>
2019-02-25 17:23:10 +0800

    优化了linux接口

--- pref/src/lib.rs
+++ pref/src/lib.rs
@@ -9,28 +9,28 @@ extern crate psutil;
 */
 pub trait SysSpecialStat {
     //获取系统可用cpu逻辑核心数
-    fn sys_cpu_runnable(&self) -> (i32, i32);
+    fn sys_cpu_runnable(&self) -> Option<(i32, i32)>;
 
     //获取系统cpu占用率
-    fn sys_cpu_usage(&self) -> f64;
+    fn sys_cpu_usage(&self) -> Option<f64>;
 
     //获取系统cpu所有逻辑核心的占用率
-    fn sys_processores_usage(&self) -> Vec<f64>;
+    fn sys_processores_usage(&self) -> Option<Vec<f64>>;
 
     //获取系统cpu详细使用信息
-    fn sys_cpu_detal(&self) -> (f64, f64, f64, f64, f64, f64, f64, f64, f64, f64);
+    fn sys_cpu_detal(&self) -> Option<(f64, f64, f64, f64, f64, f64, f64, f64, f64, f64)>;
 
     //获取系统cpu所有逻辑核心的占用率
-    fn sys_processores_detal(&self) -> Vec<(f64, f64, f64, f64, f64, f64, f64, f64, f64, f64)>;
+    fn sys_processores_detal(&self) -> Option<Vec<(f64, f64, f64, f64, f64, f64, f64, f64, f64, f64)>>;
 
     //获取系统负载系数
-    fn sys_loadavg(&self) -> (f32, f32, f32);
+    fn sys_loadavg(&self) -> Option<(f32, f32, f32)>;
 
     //获取系统虚拟内存详细信息
-    fn sys_virtual_memory_detal(&self) -> (u64, u64, u64, u64, u64, u64, u64, u64, u64, f32);
+    fn sys_virtual_memory_detal(&self) -> Option<(u64, u64, u64, u64, u64, u64, u64, u64, u64, f32)>;
 
     //获取系统交换区详细信息
-    fn sys_swap_detal(&self) -> (u64, u64, u64, u64, u64, f32);
+    fn sys_swap_detal(&self) -> Option<(u64, u64, u64, u64, u64, f32)>;
 
     //获取系统正常运行时长
     fn sys_uptime(&self) -> isize;
@@ -39,7 +39,7 @@ pub trait SysSpecialStat {
     fn process_current_pid(&self) -> i32;
 
     //获取当前进程详细信息
-    fn process_current_detal(&self) -> (u32, u32, i64, i64, f64, f64, u64, i64, u64, u64, u64, u64, u64, i32, i64, f64, String, String);
+    fn process_current_detal(&self) -> Option<(u32, u32, i64, i64, f64, f64, u64, i64, u64, u64, u64, u64, u64, i32, i64, f64, String, String)>;
 }
 
 pub mod common;
