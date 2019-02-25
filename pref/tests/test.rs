a9a3193ed7efe12d3ffd3d2f59262dbe3a67314d
vmos <vmos@fairblock.io>
2019-02-25 20:44:58 +0800

    测试

--- pref/tests/test.rs
+++ pref/tests/test.rs
@@ -206,7 +206,7 @@ fn test_psutil_() {
         println!("process start time: {}", info.15);
         println!("process command: {}", info.16);
         println!("process status: {}", info.17);
-        println!("process cwd: {}", info.19);
+        println!("process cwd: {:?}", info.19);
         println!("process cmd: {}", info.18);
     }
 
