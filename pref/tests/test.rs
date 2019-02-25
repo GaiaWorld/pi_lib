45005351c04310e89c751785ee0a70b7515262bd
vmos <vmos@fairblock.io>
2019-02-25 17:27:33 +0800

    测试

--- pref/tests/test.rs
+++ pref/tests/test.rs
@@ -102,94 +102,106 @@ fn test_psutil_() {
 
     let sys = LinuxSysStat::new(0.01);
 
-    let info = sys.sys_cpu_runnable();
-    println!("current processor: {}", info.0);
-    println!("total processor: {}", info.1);
+    if let Some(info) = sys.sys_cpu_runnable() {
+        println!("current processor: {}", info.0);
+        println!("total processor: {}", info.1);
+    }
 
     sys.sys_cpu_usage(); //预热
-    println!("cpu usage: {}", sys.sys_cpu_usage());
+    if let Some(info) = sys.sys_cpu_usage() {
+        println!("cpu usage: {}", info);
+    }
 
     let mut n = 0;
     sys.sys_processores_usage(); //预热
-    for usage in sys.sys_processores_usage() {
-        println!("processor #{} usage: {}", n, usage);
-        n += 1;
+    if let Some(infos) = sys.sys_processores_usage() {
+        for info in infos {
+            println!("processor #{} usage: {}", n, info);
+            n += 1;
+        }
     }
 
     sys.sys_cpu_detal(); //预热
-    let info = sys.sys_cpu_detal();
-    println!("cpu user usage: {}", info.0);
-    println!("cpu nice usage: {}", info.1);
-    println!("cpu system usage: {}", info.2);
-    println!("cpu idle usage: {}", info.3);
-    println!("cpu iowait usage: {}", info.4);
-    println!("cpu irq usage: {}", info.5);
-    println!("cpu soft irq usage: {}", info.6);
-    println!("cpu steal usage: {}", info.7);
-    println!("cpu guest usage: {}", info.8);
-    println!("cpu guest nice usage: {}", info.9);
+    if let Some(info) = sys.sys_cpu_detal() {
+        println!("cpu user usage: {}", info.0);
+        println!("cpu nice usage: {}", info.1);
+        println!("cpu system usage: {}", info.2);
+        println!("cpu idle usage: {}", info.3);
+        println!("cpu iowait usage: {}", info.4);
+        println!("cpu irq usage: {}", info.5);
+        println!("cpu soft irq usage: {}", info.6);
+        println!("cpu steal usage: {}", info.7);
+        println!("cpu guest usage: {}", info.8);
+        println!("cpu guest nice usage: {}", info.9);
+    }
 
     n = 0;
     sys.sys_processores_detal(); //预热
-    for info in sys.sys_processores_detal() {
-        println!("processor #{}", n);
-        println!("\tuser usage: {}", info.0);
-        println!("\tnice usage: {}", info.1);
-        println!("\tsystem usage: {}", info.2);
-        println!("\tidle usage: {}", info.3);
-        println!("\tiowait usage: {}", info.4);
-        println!("\tirq usage: {}", info.5);
-        println!("\tsoft irq usage: {}", info.6);
-        println!("\tsteal usage: {}", info.7);
-        println!("\tguest usage: {}", info.8);
-        println!("\tguest nice usage: {}", info.9);
-        n += 1;
+    if let Some(infos) = sys.sys_processores_detal() {
+        for info in infos {
+            println!("processor #{}", n);
+            println!("\tuser usage: {}", info.0);
+            println!("\tnice usage: {}", info.1);
+            println!("\tsystem usage: {}", info.2);
+            println!("\tidle usage: {}", info.3);
+            println!("\tiowait usage: {}", info.4);
+            println!("\tirq usage: {}", info.5);
+            println!("\tsoft irq usage: {}", info.6);
+            println!("\tsteal usage: {}", info.7);
+            println!("\tguest usage: {}", info.8);
+            println!("\tguest nice usage: {}", info.9);
+            n += 1;
+        }
     }
 
-    let info = sys.sys_loadavg();
-    println!("load avg: {}, {}, {}", info.0, info.1, info.2);
+    if let Some(info) = sys.sys_loadavg() {
+        println!("load avg: {}, {}, {}", info.0, info.1, info.2);
+    }
 
-    let info = sys.sys_virtual_memory_detal();
-    println!("sys total memory: {}", info.0);
-    println!("sys free memory: {}", info.1);
-    println!("sys used memory: {}", info.2);
-    println!("sys available memory: {}", info.3);
-    println!("sys active memory: {}", info.4);
-    println!("sys inactive memory: {}", info.5);
-    println!("sys buffers memory: {}", info.6);
-    println!("sys cached memory: {}", info.7);
-    println!("sys shared memory: {}", info.8);
-    println!("sys memory usage: {}", info.9);
+    if let Some(info) = sys.sys_virtual_memory_detal() {
+        println!("sys total memory: {}", info.0);
+        println!("sys free memory: {}", info.1);
+        println!("sys used memory: {}", info.2);
+        println!("sys available memory: {}", info.3);
+        println!("sys active memory: {}", info.4);
+        println!("sys inactive memory: {}", info.5);
+        println!("sys buffers memory: {}", info.6);
+        println!("sys cached memory: {}", info.7);
+        println!("sys shared memory: {}", info.8);
+        println!("sys memory usage: {}", info.9);
+    }
 
-    let info = sys.sys_swap_detal();
-    println!("sys total swap: {}", info.0);
-    println!("sys free swap: {}", info.1);
-    println!("sys used swap: {}", info.2);
-    println!("sys sin swap: {}", info.3);
-    println!("sys sout swap: {}", info.4);
-    println!("sys swap usage: {}", info.5);
+    if let Some(info) = sys.sys_swap_detal() {
+        println!("sys total swap: {}", info.0);
+        println!("sys free swap: {}", info.1);
+        println!("sys used swap: {}", info.2);
+        println!("sys sin swap: {}", info.3);
+        println!("sys sout swap: {}", info.4);
+        println!("sys swap usage: {}", info.5);
+    }
 
     println!("system uptime: {}", sys.sys_uptime());
 
     println!("current process: {}", sys.process_current_pid());
 
-    let info = sys.process_current_detal();
-    println!("process uid: {}", info.0);
-    println!("process gid: {}", info.1);
-    println!("process nice: {}", info.2);
-    println!("process priority: {}", info.3);
-    println!("process system cpu usage: {}", info.4);
-    println!("process user cpu usage: {}", info.5);
-    println!("process vm: {}", info.6);
-    println!("process rss: {}", info.7);
-    println!("process rss limit: {}", info.8);
-    println!("process minflt: {}", info.9);
-    println!("process cminflt: {}", info.10);
-    println!("process majflt: {}", info.11);
-    println!("process cmajflt: {}", info.12);
-    println!("process processor: {}", info.13);
-    println!("process threads: {}", info.14);
-    println!("process start time: {}", info.15);
-    println!("process command: {}", info.16);
-    println!("process status: {}", info.17);
+    if let Some(info) = sys.process_current_detal() {
+        println!("process uid: {}", info.0);
+        println!("process gid: {}", info.1);
+        println!("process nice: {}", info.2);
+        println!("process priority: {}", info.3);
+        println!("process system cpu usage: {}", info.4);
+        println!("process user cpu usage: {}", info.5);
+        println!("process vm: {}", info.6);
+        println!("process rss: {}", info.7);
+        println!("process rss limit: {}", info.8);
+        println!("process minflt: {}", info.9);
+        println!("process cminflt: {}", info.10);
+        println!("process majflt: {}", info.11);
+        println!("process cmajflt: {}", info.12);
+        println!("process processor: {}", info.13);
+        println!("process threads: {}", info.14);
+        println!("process start time: {}", info.15);
+        println!("process command: {}", info.16);
+        println!("process status: {}", info.17);
+    }
 }
