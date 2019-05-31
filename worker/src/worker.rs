074abe00e01cfa4866289415464a6ba89dc6ae63
vmos <vmos@fairblock.io>
2019-05-31 17:57:45 +0800

    增加了打印

--- worker/src/worker.rs
+++ worker/src/worker.rs
@@ -245,10 +245,12 @@ impl Worker {
                 }
                 wake = w;
             }
+            let now = Instant::now();
             //获取任务
             if let Some(t) = tasks.pop() {
                 //有任务
                 base_task = t;
+                println!("===> Work Get Task, time: {:?}, task: {:?}", Instant::now() - now, task);
             } else {
                 //没有任务，则重置唤醒状态，立即解锁，并处理控制状态
                 *wake = false;
