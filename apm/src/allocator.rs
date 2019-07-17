41c7b30fd2fcea6562d7b13fb82144ab575fffeb
vmos <vmos@fairblock.io>
2019-07-17 15:03:09 +0800

    移除系统内存回收方法

--- apm/src/allocator.rs
+++ apm/src/allocator.rs
@@ -3,13 +3,6 @@ use std::sync::atomic::{AtomicIsize, AtomicUsize, Ordering};
 use std::time::Instant;
 use std::f32::MAX;
 
-use common::SysStat;
-
-/*
-* 多余的空闲内存上限，单位B，默认512MB
-*/
-pub const FREE_SYSTEM_MEMORY_MAX_LIMIT: u64 = 536870912;
-
 /*
 * 当前虚拟机已分配内存
 */
@@ -101,51 +94,5 @@ pub fn all_alloced_size() -> usize {
         alloced_size()
     } else {
         alloced_size() + vm_size as usize
-    }
-}
-
-//线程安全的回收多余的空闲系统内存
-#[cfg(any(windows))]
-pub fn free_sys_mem(_: u64) -> bool {
-    true
-}
-
-//线程安全的回收多余的空闲系统内存
-#[cfg(any(unix))]
-pub fn free_sys_mem(limit: u64) -> bool {
-    let start_time = Instant::now();
-    let sys = SysStat::new().special_platform().unwrap();
-    let pid = sys.process_current_pid();
-    let current = all_alloced_size();
-    if let Some((_, _, res, _, _, _)) = sys.process_memory(pid) {
-        if let Some(sub) = res.checked_sub(current as u64) {
-            if sub >= limit {
-                //多余的空闲内存已达限制，则回收多余的空闲内存
-                unsafe {
-                    if dukc_manual_free() == 0 {
-                        println!("===> Collect System Memory Ok, Free Failed, current: {}, before real: {}, after real: {}, limit: {}, time: {:?}", current, res, after_res, limit, Instant::now() - start_time);
-                        false
-                    } else {
-                        let after_res = if let Some((_, _, after, _, _, _)) = sys.process_memory(pid) {
-                            after
-                        } else {
-                            0
-                        };
-                        println!("===> Collect System Memory Ok, Free Ok, current: {}, before real: {}, after real: {}, limit: {}, time: {:?}", current, res, after_res, limit, Instant::now() - start_time);
-                        true
-                    }
-                }
-            } else {
-                //多余的空闲内存未达限制，则忽略
-                false
-            }
-        } else {
-            //内存占用异常，则回收失败
-            println!("!!!> Collect System Memory Failed, current: {}, real: {}, limit: {}", current, res, limit);
-            false
-        }
-    } else {
-        //获取不到当前进程内存占用，则回收失败
-        false
     }
 }
