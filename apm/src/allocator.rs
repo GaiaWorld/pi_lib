1f813240f435acadfceda3c4c72cb4eca5e7fb1f
vmos <vmos@fairblock.io>
2019-07-17 14:18:33 +0800

    增加了回收系统内存的方法

--- apm/src/allocator.rs
+++ apm/src/allocator.rs
@@ -1,6 +1,19 @@
 use std::alloc::{System, GlobalAlloc, Layout};
 use std::sync::atomic::{AtomicIsize, AtomicUsize, Ordering};
+use std::time::Instant;
 use std::f32::MAX;
+
+use common::SysStat;
+
+/*
+* 多余的空闲内存上限，单位B，默认512MB
+*/
+pub const FREE_SYSTEM_MEMORY_MAX_LIMIT: u64 = 536870912;
+
+/*
+* 当前虚拟机已分配内存
+*/
+pub static VM_ALLOCATED: AtomicIsize = AtomicIsize::new(0);
 
 /*
 * 当前最大已分配内存限制，默认8GB
@@ -11,11 +24,6 @@ static MAX_ALLOCATED_LIMIT: AtomicUsize = AtomicUsize::new(8589934590);
 * 当前已分配内存
 */
 static ALLOCATED: AtomicUsize = AtomicUsize::new(0);
-
-/*
-* 当前虚拟机已分配内存
-*/
-pub static VM_ALLOCATED: AtomicIsize = AtomicIsize::new(0);
 
 /*
 * 计数的系统内存分配器
@@ -93,5 +101,50 @@ pub fn all_alloced_size() -> usize {
         alloced_size()
     } else {
         alloced_size() + vm_size as usize
+    }
+}
+
+//线程安全的回收多余的空闲系统内存
+#[cfg(any(windows))]
+pub fn free_sys_mem(_: u64) -> bool {
+    true
+}
+
+//线程安全的回收多余的空闲系统内存
+#[cfg(any(unix))]
+pub fn free_sys_mem(limit: u64) -> bool {
+    let start_time = Instant::now();
+    let sys = SysStat::new().special_platform().unwrap();
+    let pid = sys.process_current_pid();
+    let current = all_alloced_size();
+    if let Some((_, _, res, _, _, _)) = sys.process_memory(pid) {
+        if let Some(sub) = res.checked_sub(current as u64) {
+            if sub >= limit {
+                //多余的空闲内存已达限制，则回收多余的空闲内存
+                unsafe {
+                    if dukc_manual_free() == 0 {
+                        false
+                    } else {
+                        let after_res = if let Some((_, _, after, _, _, _)) = sys.process_memory(pid) {
+                            after
+                        } else {
+                            -1
+                        };
+                        println!("===> Collect System Memory Ok, current: {}, before real: {}, after real: {}, limit: {}, time: {:?}", current, res, after_res, limit, Instant::now() - start_time);
+                        true
+                    }
+                }
+            } else {
+                //多余的空闲内存未达限制，则忽略
+                false
+            }
+        } else {
+            //内存占用异常，则回收失败
+            println!("!!!> Collect System Memory Failed, current: {}, real: {}, limit: {}", current, res, limit);
+            false
+        }
+    } else {
+        //获取不到当前进程内存占用，则回收失败
+        false
     }
 }
