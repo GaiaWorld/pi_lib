41e3ef30df5499f4f8abccec42279c7c6d72b2fe
moyy <moyy@yineng.com>
2019-08-12 20:58:36 +0800

    添加FxHashSet类型

--- fx_hashmap/src/lib.rs
+++ fx_hashmap/src/lib.rs
@@ -6,3 +6,4 @@ use fxhash::FxHasher32;
 
 type FxBuildHasher32 = BuildHasherDefault<FxHasher32>;
 pub type FxHashMap32<K, V> = std::collections::HashMap<K, V, FxBuildHasher32>;
+pub type FxHashSet32<K> = std::collections::HashSet<K, FxBuildHasher32>;
