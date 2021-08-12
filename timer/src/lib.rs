e8f41f61b67c7f71de642aa027e1f498f1d92dfb
zuon <zuon@yinengyun.com>
2021-08-12 13:08:08 +0800

    移除不需要的依赖

--- timer/src/lib.rs
+++ timer/src/lib.rs
@@ -1,382 +1,161 @@
-#![feature(integer_atomics)]
+//! 不可撤销的定时器
 
-extern crate atom;
-extern crate wheel;
-extern crate time;
+use std::{cmp::Reverse, fmt};
 
-#[macro_use]
-extern crate lazy_static;
+use ext_heap::{empty as heap_empty, ExtHeap};
+use wheel::{TimeoutItem, Wheel};
 
-use std::sync::Arc;
-use std::sync::Mutex;
-use std::thread;
-use std::time::{Duration};
-use std::sync::atomic::{AtomicUsize, AtomicU64};
-use std::mem::{transmute};
-use std::marker::Send;
-use std::fmt::{Debug, Formatter, Result as FResult};
-
-use atom::Atom;
-use wheel::slab_wheel::Wheel;
-use wheel::wheel::Item;
-use time::{run_millis};
-
-pub trait Runer{
-    fn run(self, index: usize);
+/// 不可撤销的定时器
+pub struct Timer<T, const N0: usize, const N: usize, const L: usize> {
+    wheel: Wheel<T, N0, N, L>, // 定时轮
+    heap: ExtHeap<Reverse<TimeoutItem<T>>>, // 最小堆
+    add_count: usize,
+    remove_count: usize,
+    roll_count: u64,
 }
 
-impl<T: 'static + Send + Runer> Timer<T>{
-    pub fn new(clock_ms: u64) -> Self {
-        Timer(Arc::new(Mutex::new(TimerImpl::new(clock_ms))))
+impl<T: fmt::Debug, const N0: usize, const N: usize, const L: usize> fmt::Debug
+    for Timer<T, N0, N, L>
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Timer")
+            .field("wheel", &self.wheel)
+            .field("heap", &self.heap)
+            .field("add_count", &self.add_count)
+            .field("remove_count", &self.remove_count)
+            .field("roll_count", &self.roll_count)
+            .finish()
     }
-
-    #[allow(unused_must_use)]
-    pub fn run(&self){
-        let s = self.0.clone();
-		thread::Builder::new()
-            .name("Timer".to_string())
-            .spawn(move ||{
-                let mut sleep_time = s.lock().unwrap().clock_ms;
-				let start_time = run_millis();
-                loop {
-                    thread::sleep(Duration::from_millis(sleep_time));
-                    let mut now = run_millis();
-                    run_zero(&s);//运行0毫秒任务
-                    loop {
-                        let mut r = {
-							let mut s = s.lock().unwrap();
-							let next_roll_time = s.clock_ms + s.wheel.get_time() + start_time;
-                            match now >= next_roll_time{
-                                true => s.wheel.roll(),
-                                false => {
-                                    sleep_time = next_roll_time - now;
-                                    break;
-                                }
-                            }
-						};
-						
-                        run_task(&s, &mut r);
-						run_zero(&s);//运行0毫秒任务
-						now = run_millis();
-                    }
-                }
-		});
-	}
-
-    pub fn set_timeout(&self, elem: T, ms: u32) -> usize{
-
-        let mut lock = self.0.lock().unwrap();
-		let time =  lock.wheel.get_time();
-		lock.wheel.insert(Item{elem: elem, time_point: time + (ms as u64)})
-	}
-
-    pub fn cancel(&self, index: usize) -> Option<T>{
-        let mut lock = self.0.lock().unwrap();
-		match lock.wheel.try_remove(index) {
-			Some(v) => {
-                Some(v.elem)
-            },
-			None => {None},
-		}
-	}
-
-    pub fn clear(&self){
-        self.0.lock().unwrap().clear();
-	}
 }
-
-pub struct TimerImpl<T: Send + Runer>{
-	wheel: Wheel<T>,
-	_statistics: Statistics,
-	clock_ms: u64,
-}
-
-impl<T: Send + Runer> TimerImpl<T>{
-	pub fn new(mut clock_ms: u64) -> Self{
-        if clock_ms < 10{
-            clock_ms = 10;
+impl<T, const N0: usize, const N: usize, const L: usize> Default for Timer<T, N0, N, L> {
+    fn default() -> Self {
+        Timer {
+            wheel: Default::default(),
+            heap: Default::default(),
+            add_count: 0,
+            remove_count: 0,
+            roll_count: 0,
         }
-		TimerImpl{
-			wheel: Wheel::new(), 
-			_statistics: Statistics::new(),
-			clock_ms: clock_ms,
-		}
-    }
-
-    pub fn clear(&mut self){
-        self.wheel.clear();
-	}
-}
-
-pub struct FuncRuner(usize, usize);
-
-impl FuncRuner{
-    pub fn new(f: Box<dyn FnOnce()>) -> Self {
-        unsafe { transmute(f) }
     }
 }
 
-impl Runer for FuncRuner {
-    fn run(self, _index: usize){
-        let func: Box<dyn FnOnce()> = unsafe { transmute((self.0, self.1)) };
-        func();
+impl<T, const N0: usize, const N: usize, const L: usize> Timer<T, N0, N, L> {
+    /// 获得添加任务数量
+    pub fn add_count(&self) -> usize {
+        self.add_count
     }
-}
-
-impl Debug for FuncRuner {
-	fn fmt(&self, fmt: &mut Formatter) -> FResult {
-        write!(fmt,"FuncRuner")
+    /// 获得移除任务数量
+    pub fn remove_count(&self) -> usize {
+        self.remove_count
     }
-}
-
-
-lazy_static! {
-	pub static ref TIMER: Timer<FuncRuner> = Timer::new(10);
-}
-
-pub struct Timer<T: 'static + Send + Runer>(pub Arc<Mutex<TimerImpl<T>>>);
-
-impl<T: 'static + Send + Runer> Clone for Timer<T> {
-    fn clone(&self) -> Self{
-        Timer(self.0.clone())
+    /// 获得滚动次数
+    pub fn roll_count(&self) -> u64 {
+        self.roll_count
     }
-}
-
-#[derive(Clone)]
-struct Statistics {
-	pub all_count: Arc<AtomicUsize>,
-    pub cancel_count: Arc<AtomicUsize>,
-	pub run_count: Arc<AtomicUsize>,
-	pub run_time: Arc<AtomicU64>,
-}
-
-impl Statistics{
-	pub fn new() -> Statistics{
-		Statistics{
-			all_count: Arc::new(AtomicUsize::new(0)),
-            cancel_count: Arc::new(AtomicUsize::new(0)),
-			run_count: Arc::new(AtomicUsize::new(0)),
-			run_time: Arc::new(AtomicU64::new(0)),
-		}
-	}
-}
-
-
-fn run_zero<T: Send + Runer>(timer: &Arc<Mutex<TimerImpl<T>>>){
-    loop {
-        let mut r = {
-            let mut s = timer.lock().unwrap();
-            match s.wheel.zero_size() > 0{
-                true => s.wheel.get_zero(),
-                false => {
+    /// 放入一个定时任务
+    pub fn push(&mut self, timeout: usize, el: T) {
+        self.add_count += 1;
+        if let Some(r) = self.wheel.push(TimeoutItem::new(timeout, el)) {
+            // 没有放入的定时任务的时间已经被转换成绝对时间，放入堆中
+            self.heap.push(Reverse(r), &mut (), heap_empty);
+        }
+    }
+    /// 弹出一个定时任务
+    /// * `now` 当前时间
+    /// * @return `Option<T>` 弹出的定时元素
+    pub fn pop(&mut self, now: u64) -> Option<T> {
+        loop {
+            if let Some(r) = self.wheel.pop() {
+                self.remove_count += 1;
+                return Some(r.el)
+            }
+            if self.roll_count >= now {
+                return None
+            }
+            self.roll();
+        }
+    }
+    /// 判断指定时间内是否还有定时任务
+    pub fn is_ok(&mut self, now: u64) -> bool {
+        loop {
+            if !self.wheel.is_cur_over() {
+                return true
+            }
+            if self.roll_count >= now {
+                return false
+            }
+            self.roll();
+        }
+    }
+    /// 轮滚动 - 向后滚动一个最小粒度, 可能会造成轮的逐层滚动。如果滚动到底，则修正堆上全部的定时任务，并将堆上的到期任务放入轮中
+    pub fn roll(&mut self) {
+        self.roll_count += 1;
+        if self.wheel.roll() {
+            // 修正堆上全部的定时任务
+            for i in 0..self.heap.len() {
+                unsafe { self.heap.get_unchecked_mut(i).0.timeout -= self.wheel.max_time() };
+            }
+            // 如果滚到轮的最后一层的最后一个， 则将堆上的到期任务放入轮中
+            // 检查堆顶的最近的任务
+            while let Some(it) = self.heap.peek() {
+                // 判断任务是否需要放入轮中
+                if it.0.timeout >= self.wheel.max_time() {
                     break;
                 }
+                let it = self.heap.pop(&mut (), heap_empty).unwrap();
+                // 时间已经修正过了，可以直接放入定时轮中
+                self.wheel.push(it.0);
             }
-        };
-        run_task(timer, &mut r);
-        r.clear();
-        timer.lock().unwrap().wheel.set_zero_cache(r);
-    }
-}
-
-//执行任务，返回任务执行完的时间
-fn run_task<T: Send + Runer>(_timer: &Arc<Mutex<TimerImpl<T>>>, r: &mut Vec<(Item<T>, usize)>){
-	let mut j = r.len();
-    for _ in 0..r.len(){
-        j -= 1;
-        let e = r.remove(j);
-        e.0.elem.run(e.1);
-    }
-}
-#[test]
-fn test(){
-    TIMER.run();
-    //thread::sleep(Duration::from_millis(8));
-    //let now = now_millis();
-    TIMER.set_timeout(FuncRuner::new(Box::new(move||{
-        println!("test timer Success");
-	})), 10);
-	//let index = TIMER.set_timeout(Box::new(f), 1000);
-    //println!("index-------------{}", index.load(Ordering::Relaxed));
-	thread::sleep(Duration::from_millis(500));
-}
-
-#[cfg(test)]
-extern crate rand;
-#[cfg(test)]
-use std::sync::atomic::Ordering;
-
-// 测试定时任务弹出数量是否和插入数量保持一致
-#[test]
-fn test_count() {
-    use rand::thread_rng;
-    use rand::Rng;
-	use rand::seq::SliceRandom;
-	use std::collections::HashMap;
-
-    TIMER.run();
-    let mut rng = thread_rng();
-
-    let count = Arc::new(AtomicUsize::new(0));
-
-	let mut timer_refs = vec![];
-	let mut timer_map = HashMap::new();
-	let total = 100000;
-    for _i in 1..total + 1 {
-		let count = count.clone();
-		let t = rng.gen_range(10, 5000);
-        let timer_ref  =TIMER.set_timeout(FuncRuner::new(Box::new(move || {
-            count.fetch_add(1, Ordering::SeqCst);
-        })), t);
-
-		timer_refs.push(timer_ref);
-		timer_map.insert(timer_ref, true);
-	}
-
-
-	let cancel: Vec<usize> = timer_refs.choose_multiple(&mut rng, 50).cloned().collect();
-	// let cancel = Vec::new();
-
-    println!("shuffled timer_refs = {:?}", cancel);
-
-	let mut cancel_success = Vec::new();
-	let mut cancel_fail = Vec::new();
-    for c in cancel {
-        if let Some(_) = TIMER.cancel(c) {
-			// println!("cancel success {:?}", c);
-			cancel_success.push(c);
-        } else {
-			// println!("cancel failed {:?}", c);
-			cancel_fail.push(c);
         }
-        thread::sleep(Duration::from_millis(rng.gen_range(10, 100)));
     }
-
-    thread::sleep(Duration::from_millis(5100));
-	println!("run: {:?}, total: {:?}, cancel_success: {}, cancel_fail:{}", count, total, cancel_success.len(), cancel_fail.len());
 }
-
 
 // 测试定时器得延时情况
-#[test]
-fn test_timer_delay() {
-    use rand::thread_rng;
-    use rand::Rng;
+#[cfg(test)]
+mod test_mod {
+    extern crate pcg_rand;
+    extern crate rand_core;
 
-	TIMER.run();
-    let mut rng = thread_rng();
-    for _i in 1..100000 {
-		let t = rng.gen_range(10, 10000);
-		let time = run_millis();
-        TIMER.set_timeout(FuncRuner::new(Box::new(move || {
-			if run_millis() as isize - time as isize - t as isize > 11 || -11 > run_millis() as isize - time as isize - t as isize {
-				println!("task delay================{}, {}", run_millis() as isize - time as isize - t as isize, t as u64 + time);
-			}
-		})), t);
-	}
-	
-	thread::sleep(Duration::from_millis(11000));
+    use std::{
+        thread,
+        time::{Duration, Instant},
+    };
+
+    use self::rand_core::{RngCore, SeedableRng};
+    use crate::*;
+
+    #[test]
+    fn test() {
+        let mut timer: Timer<(u64, u64), 128, 16, 1> = Default::default();
+        let mut rng = pcg_rand::Pcg32::seed_from_u64(22222);
+        let start = Instant::now();
+        println!("max_time:{}", timer.wheel.max_time());
+        for i in 1..100000 {
+            let t = (rng.next_u32() % 16100) as u64;
+            let now = Instant::now();
+            let tt = now.duration_since(start).as_millis() as u64;
+            if i < 100 {
+                println!("push: timeout:{} realtime:{:?}", t, (i, t + tt));
+                timer.push(t as usize, (i, t + tt));
+            }
+            if t == 9937 || t == 15280 {
+                println!("{:?}", timer.wheel);
+            }
+            //while let Some(it) = timer.pop(tt) {
+            while timer.is_ok(tt) {
+                let it = timer.pop(tt).unwrap();
+                println!("ppp:{:?}, now:{}", it, tt);
+            }
+            if i > 100 && timer.add_count == timer.remove_count {
+                //println!("vec:{:?}", vec);
+                println!(
+                    "return: add_count:{:?}",
+                    timer.add_count
+                );
+                return;
+            }
+            thread::sleep(Duration::from_millis(1 as u64));
+        }
+    }
+
 }
-
-// test_timer_delay中如果存在某个时间延迟较多，则可在此测试中单独测试该时间任务是否准时
-#[test] 
-fn test_timer_delay_single() {
-	TIMER.set_timeout(FuncRuner::new(Box::new(move || {
-		println!("task================{}", run_millis());
-	})), 6692);
-
-	println!("start================{}", run_millis());
-	TIMER.run();
-	thread::sleep(Duration::from_millis(11000));
-}
-
-// #[test]
-// fn test_timer2() {
-//     use rand::thread_rng;
-//     use rand::Rng;
-// 	use rand::seq::SliceRandom;
-// 	use std::collections::HashMap;
-
-// 	TIMER.run();
-//     let mut rng = thread_rng();
-
-//     let count = Arc::new(AtomicUsize::new(0));
-
-// 	// let mut timer_refs = Arc::new(Mutex::new(vec![]));
-// 	// let mut timer_map = HashMap::new();
-
-// 		let count = count.clone();
-// 		let t = 9231;
-// 		let time = run_millis();
-//         let timer_ref  =TIMER.set_timeout(FuncRuner::new(Box::new(move || {
-// 			// count.fetch_add(1, Ordering::SeqCst);
-// 			// t1.lock().unwrap().push(( run_millis() as isize - time as isize - t as isize, t, run_millis()));
-// 			println!("==============={}, {}, now:{}", run_millis() as isize - time as isize - t as isize, t, run_millis());
-// 		})), t);
-	
-	
-// 	// 	timer_refs.push(timer_ref);
-// 	// 	timer_map.insert(timer_ref, true);
-// 	// }
-
-//     // thread::sleep(Duration::from_millis(11000));
-// 	// println!("count = {:?}, {:?}", count, TIMER_RUN_COUNT.get());
-
-// 	// let time = run_millis();
-// 	// let timer_ref  =TIMER.set_timeout(FuncRuner::new(Box::new(move || {
-		
-// 	// 	println!("=================={}", run_millis() - time);
-// 	// })), 10000);
-	
-// 	thread::sleep(Duration::from_millis(13000));
-// 	println!("timer_refs1======================{:?}", timer_refs1.lock().unwrap().as_slice());
-// }
-
-// #[test]
-// fn test_wheel() {
-//     use rand::thread_rng;
-//     use rand::Rng;
-// 	use rand::seq::SliceRandom;
-// 	use std::collections::HashMap;
-// 	use wheel::wheel::Item;
-// 	let mut rng = thread_rng();
-// 	let mut timer_refs = Arc::new(Mutex::new(vec![]));
-
-// 	let mut r = Wheel::new();
-// 	for i in 1..1000 {
-// 		let t = rng.gen_range(10, 10000);
-// 		let time = run_millis();
-// 		let t1 = timer_refs.clone();
-// 		r.insert(Item{
-// 			elem: FuncRuner::new(Box::new(move || {
-// 				// t1.lock().unwrap().push(( run_millis() as isize - time as isize - t as isize, t));
-// 			})),
-// 			time_point: run_millis() + t,
-// 		});
-// 	}
-
-// 	let mut count=  0;
-// 	while count < 1000 {
-// 		count +=  1;
-// 		let time = run_millis();
-// 		r.roll();
-// 		timer_refs.lock().unwrap().push( run_millis() as isize - time as isize);
-// 	}
-// 	// let mut timer_map = HashMap::new();
-    
-	
-// 	// 	timer_refs.push(timer_ref);
-// 	// 	timer_map.insert(timer_ref, true);
-// 	// }
-
-//     // thread::sleep(Duration::from_millis(11000));
-// 	// println!("count = {:?}, {:?}", count, TIMER_RUN_COUNT.get());
-
-// 	// let time = run_millis();
-// 	// let timer_ref  =TIMER.set_timeout(FuncRuner::new(Box::new(move || {
-		
-// 	// 	println!("=================={}", run_millis() - time);
-// 	// })), 10000);
-// 	thread::sleep(Duration::from_millis(13000));
-// 	println!("======================{:?}", timer_refs);
-// }
--- weight/src/lib.rs
+++ weight/src/lib.rs
@@ -70,17 +70,8 @@
 //! random=1; 取到2左节点， 1 <= 1, 弹出2左节点;
 //!
 
-#![warn(type_alias_bounds)]
-#![allow(missing_docs)]
 
-extern crate alloc;
-
-extern crate ext_heap;
-
-use std::{
-    cmp::{Ord, Ordering, PartialOrd},
-    fmt,
-};
+use std::{cmp::{Ord, Ordering, PartialOrd}, fmt, ops::{Deref, DerefMut}};
 
 use ext_heap::*;
 
@@ -131,11 +122,11 @@ impl<T> Ord for WeightItem<T> {
 }
 
 impl<T> WeightItem<T> {
-    pub fn new(weight: usize, item: T) -> Self {
+    pub fn new(weight: usize, el: T) -> Self {
         WeightItem {
             weight,
             amount: weight,
-            el: item,
+            el,
         }
     }
     pub fn weight(&self) -> usize {
@@ -145,22 +136,38 @@ impl<T> WeightItem<T> {
         self.amount
     }
 }
-pub type WeightHeap<T> = ExtHeap<WeightItem<T>>;
 
-/// 增加可删除堆的操作接口
-pub trait HeapAction<T> {
-    /// 寻找指定权重的元素的位置，要求该权重值<总权重
-    fn find_weight(&self, weight: usize) -> usize;
-    /// 移除指定位置的元素，并维护权重统计
-    fn remove_index(&mut self, index: usize) -> WeightItem<T>;
-    /// 修改指定位置元素的权重，并维护权重统计
-    fn modify_weight(&mut self, index: usize, weight: usize) -> Option<usize>;
-    /// 放入元素并维护权重统计
-    fn push_weight(&mut self, item: WeightItem<T>);
+/// 权重堆
+pub struct WeightHeap<T>(ExtHeap<WeightItem<T>>);
+
+impl<T: fmt::Debug> fmt::Debug for WeightHeap<T>{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_tuple("WeightHeap").field(&self.0).finish()
+    }
 }
 
-impl<T: fmt::Debug> HeapAction<T> for ExtHeap<WeightItem<T>> {
-    fn find_weight(&self, mut weight: usize) -> usize {
+impl<T> Default for WeightHeap<T> {
+    fn default() -> Self {
+        WeightHeap(Default::default())
+    }
+}
+impl<T> Deref for WeightHeap<T> {
+	type Target = ExtHeap<WeightItem<T>>;
+    #[inline(always)]
+	fn deref(&self) -> &ExtHeap<WeightItem<T>> {
+		&self.0
+	}
+}
+impl<T> DerefMut for WeightHeap<T> {
+    #[inline(always)]
+	fn deref_mut(&mut self) -> &mut ExtHeap<WeightItem<T>> {
+		&mut self.0
+	}
+}
+
+impl<T> WeightHeap<T> {
+    /// 寻找指定权重的元素的位置，要求该权重值<总权重
+    pub fn find_weight(&self, mut weight: usize) -> usize {
         let mut index = 0;
         let arr = self.as_slice();
         while weight > arr[index].weight {
@@ -175,34 +182,38 @@ impl<T: fmt::Debug> HeapAction<T> for ExtHeap<WeightItem<T>> {
         }
         index
     }
-
-    fn remove_index(&mut self, index: usize) -> WeightItem<T> {
+    /// 移除指定位置的元素，并维护权重统计
+    pub fn remove_index<A>(&mut self, index: usize, arg: &mut A, func: fn(&mut A, &mut [WeightItem<T>], usize)) -> WeightItem<T> {
         if self.len() == 1 {
-            return self.remove(index, &mut |_, _| {})
+            return self.remove(index, arg, func)
         }
-        let mut cur = self.len() - 1;
+        let cur = self.len() - 1;
         // 先修正最后一个节点所在叶到根路径的全权重值
         let weight = self.as_slice()[cur].weight;
-        fix_parent_weight(self, cur, |el| el.amount -= weight);
-        let item = self.remove(index, &mut |_, loc| {
-            *(&mut cur) = loc;
-        });
+        fix_parent_weight(self, cur, -(weight as isize));
+        if index == cur {
+            return self.remove(index, arg, func)
+        }
+        let mut context = Context{cur, arg, func};
+        let item = self.remove(index, &mut context, Context::set_index);
         // 如果调整的最后一个节点放到的不是当前位置，则修复叶到根的修复总权重值
-        if cur > index {
-            fix_weight(self, cur, index);
+        if context.cur > index {
+            fix_weight(self, context.cur, index);
+        }else {
+            fix_index_weight(self, index);
         }
         // 向上递归修改每个父节点的总权重值
         if index > 0 {
             if weight > item.weight {
-                fix_parent_weight(self, index, |el| el.amount += weight - item.weight);
+                fix_parent_weight(self, index, (weight - item.weight) as isize);
             } else if weight < item.weight {
-                fix_parent_weight(self, index, |el| el.amount -= item.weight - weight);
+                fix_parent_weight(self, index, weight as isize - item.weight as isize);
             }
         }
         item
     }
-
-    fn modify_weight(&mut self, index: usize, weight: usize) -> Option<usize> {
+    /// 修改指定位置元素的权重，并维护权重统计
+    pub fn modify_weight<A>(&mut self, index: usize, weight: usize, arg: &mut A, func: fn(&mut A, &mut [WeightItem<T>], usize)) -> Option<usize> {
         if index >= self.len() {
             return None;
         }
@@ -212,46 +223,56 @@ impl<T: fmt::Debug> HeapAction<T> for ExtHeap<WeightItem<T>> {
             // 节点向下落
             el.weight = weight;
             el.amount -= old_weight - weight;
-            let cur = self.repair(index, weight.cmp(&old_weight), &mut |_, _| {});
+            let cur = self.repair(index, Ordering::Less, arg, func);
             if cur > index {
                 fix_weight(self, cur, index);
             }
             // 向上递归修改每个父节点的总权重值
             if index > 0 {
-                fix_parent_weight(self, index, |el| el.amount -= old_weight - weight);
+                fix_parent_weight(self, index, weight as isize - old_weight as isize);
             }
             Some(cur)
         } else if old_weight < weight {
             // 节点向上升
             el.weight = weight;
             el.amount += weight - old_weight;
-            let cur = self.repair(index, weight.cmp(&old_weight), &mut |_, _| {});
+            let cur = self.repair(index, Ordering::Greater, arg, func);
             // 如果放到的不是最后一个位置，则修复叶到根的修复总权重值
             if index > cur {
                 fix_weight(self, index, cur);
             }
             // 向上递归修改每个父节点的总权重值
             if cur > 0 {
-                fix_parent_weight(self, cur, |el| el.amount += weight - old_weight);
+                fix_parent_weight(self, cur, (weight - old_weight) as isize);
             }
             Some(cur)
         } else {
             Some(index)
         }
     }
-
-    fn push_weight(&mut self, item: WeightItem<T>) {
+    /// 放入元素并维护权重统计
+    pub fn push_weight<A>(&mut self, weight: usize, el: T, arg: &mut A, func: fn(&mut A, &mut [WeightItem<T>], usize)) {
         let old = self.len();
-        let weight = item.weight;
-        let cur = self.push(item, &mut |_, _| {});
+        let cur = self.push(WeightItem::new(weight, el), arg, func);
         // 如果放到的不是最后一个位置，则修复叶到根的修复总权重值
         if old > cur {
             fix_weight(self, old, cur);
         }
         // 向上递归修改每个父节点的总权重值
         if cur > 0 {
-            fix_parent_weight(self, cur, |el| el.amount += weight);
+            fix_parent_weight(self, cur, weight as isize);
         }
+    }
+}
+struct Context<'a, A: 'a, T: 'a> {
+    cur: usize,
+    arg: &'a mut A,
+    func: fn(&mut A, &mut [WeightItem<T>], usize),
+}
+impl<'a, A: 'a, T: 'a> Context<'a, A, T> {
+    fn set_index(&mut self, arr: &mut [WeightItem<T>], loc: usize) {
+        self.cur = loc;
+        (self.func)(self.arg, arr, loc)
     }
 }
 // 从叶到根，修复总权重值
@@ -277,17 +298,7 @@ fn fix_weight<T>(heap: &mut WeightHeap<T>, mut child: usize, end: usize) {
         el.amount = el.weight + w;
     } else {
         // 最后一层和倒数第二层，可能没有左右子节点
-        let left = child + child + 1;
-        let mut w = 0;
-        if left < len {
-            w = heap.as_slice()[left].amount;
-            let right = left + 1;
-            if right < len {
-                w += heap.as_slice()[right].amount;
-            }
-        }
-        let el = unsafe { heap.get_unchecked_mut(child) };
-        el.amount = el.weight + w;
+        fix_index_weight(heap, child);
     }
     if child == 0 {
         return;
@@ -305,15 +316,30 @@ fn fix_weight<T>(heap: &mut WeightHeap<T>, mut child: usize, end: usize) {
         child = (child - 1) / 2;
     }
 }
+//修复指定位置的节点的总权重值，可能没有左右子节点
+fn fix_index_weight<T>(heap: &mut WeightHeap<T>, index: usize) {
+    let left = index + index + 1;
+    let mut w = 0;
+    if left < heap.len() {
+        w = heap.as_slice()[left].amount;
+        let right = left + 1;
+        if right < heap.len() {
+            w += heap.as_slice()[right].amount;
+        }
+    }
+    let el = unsafe { heap.get_unchecked_mut(index) };
+    el.amount = el.weight + w;
+}
 // 向上递归修改每个父节点的总权重值
 fn fix_parent_weight<T>(
     heap: &mut WeightHeap<T>,
     mut index: usize,
-    f: impl Fn(&mut WeightItem<T>),
+    fix: isize,
 ) {
     loop {
         index = (index - 1) / 2;
-        f(unsafe { heap.get_unchecked_mut(index) });
+        let el = unsafe { heap.get_unchecked_mut(index) };
+        el.amount = (el.amount as isize + fix) as usize;
         if index == 0 {
             break;
         }
@@ -327,56 +353,61 @@ fn test() {
     extern crate rand_core;
 
     use rand_core::{RngCore,SeedableRng};
-    let mut rng = pcg_rand::Pcg32::seed_from_u64(12388843);
-
-    let mut heap: WeightHeap<usize> = WeightHeap::new();
+    let seed = 12843;
+    let mut rng = pcg_rand::Pcg32::seed_from_u64(seed);
+    println!("start seed:{}", seed);
+    let mut heap: WeightHeap<usize> = Default::default();
     // let vec = vec![1, 10, 6, 5, 9, 4, 4, 4, 3, 7, 100, 90, 2, 15, 8, 22];
     let mut vec = Vec::new();
-    for _ in 0..100 {
+    for _ in 0..1000 {
         vec.push(rng.next_u32() as usize);
     }
     let mut sum = 0;
     // 测试添加权重
     for i in vec.clone() {
         sum += i;
-        heap.push_weight(WeightItem::new(i, i));
+        heap.push_weight(i, i, &mut (), empty);
         assert_eq!(heap.peek().unwrap().amount, sum);
     }
-    check(&heap);
+    check(&heap, false);
     println!("push_weight Ok");
     // 测试调整权重
     for i in 0..heap.len() {
         let old = heap.as_slice()[heap.len() - i - 1].weight;
-        heap.modify_weight(heap.len() - i - 1, old + i + 1);
-        check(&heap);
+        heap.modify_weight(heap.len() - i - 1, old + i + 1, &mut (), empty);
+        check(&heap, false);
     }
     println!("modify_weight Ok");
     // 测试删除权重
     while heap.len() > 0 {
-        heap.remove_index(heap.len() / 2);
-        check(&heap);
+        heap.remove_index(heap.len() / 2, &mut (), empty);
+        check(&heap, false);
     }
     println!("remove_index Ok");
     // 测试根据权重弹出元素
     let mut sum = 0;
+    let mut c = 0;
     for i in vec.clone() {
         sum += i;
-        heap.push_weight(WeightItem::new(i, i));
+        heap.push_weight(i, c, &mut (), empty);
         assert_eq!(heap.peek().unwrap().amount, sum);
+        c += 1;
     }
-    check(&heap);
-    for i in 0..heap.len() {
-        sum += i;
+    check(&heap, false);
+    for _i in 0..heap.len() {
         let amount = heap.peek().unwrap().amount;
-        let index = heap.find_weight(amount - 1);
-        heap.remove_index(index);
-        println!("pop_weight {:?}, index: {}", amount /2, index);
-        check(&heap);
+        let index = heap.find_weight(rng.next_u32() as usize % amount);
+        let _r = heap.remove_index(index, &mut (), empty);
+        //println!("pop_weight {:?}, weight: {}, index: {}", r, amount /2, index);
+        check(&heap, false);
     }
+    println!("pop_weight Ok");
 }
 #[cfg(test)]
-fn check(heap: &WeightHeap<usize>) {
-    //println!("{:?}", heap);
+fn check(heap: &WeightHeap<usize>, show: bool) {
+    if show {
+        println!("{:?}", heap);
+    }
     let arr = heap.as_slice();
     for i in 0..arr.len() {
         let left = i + i + 1;
